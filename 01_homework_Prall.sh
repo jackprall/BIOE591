b44a0fe4939819f6613e8eb15d57df50f3d07937
jackprall <prall.jack17@gmail.com>
2026-01-29 13:58:35 -0700

    add first files

---
+++ 01_homework_Prall.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd ~/week_1
+
+mkdir -p fastq fasta metadata
+
+mv *.fastq.gz fastq/
+mv *.fasta fasta/
+mv *.csv metadata/
+
+ls -1 fastq/ | wc -l
+ls -1 fasta/ | wc -l
+ls -1 metadata/ | wc -l
