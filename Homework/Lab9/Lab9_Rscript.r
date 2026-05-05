0463c40c428bf98af966256f7c95f3b5006bf236
jackprall <prall.jack17@gmail.com>
2026-05-05 13:52:45 -0600

    End of year catch up, part 2

---
+++ Homework/Lab9/Lab9_Rscript.r
@@ -0,0 +1,15 @@
+# This script was write for BIOE591 Lab 9
+# By Jack Pralll
+
+# library fun
+install.packages("related", repos = "https://R-Forge.R-project.org")
+install.packages("adegenet")
+install.packages("vcfR")
+install.packages("pegas")
+library(related)
+library(adegenet)
+library(vcfR)
+library(pegas)
+library(tidyr)
+library(reshape2)
+library(ggplot2)
